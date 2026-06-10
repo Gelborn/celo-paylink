865c8d3eae719fc793ade6efb81e7db26952c55e
Gelborn <felipe-grasnievicz@hotmail.com>
2026-06-10 09:21:48 -0300

    Add dashboard page metadata

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,6 +1,17 @@
+import type { Metadata } from "next";
 import { DashboardShell } from "../../components/dashboard-shell";
 import { getDefaultChainId } from "../../lib/chains";
 import { publicEnv } from "../../lib/env";
+
+export const metadata: Metadata = {
+  title: "PayLink dashboard",
+  description:
+    "Manage a MiniPay PayLink public profile and create prefilled request links.",
+  robots: {
+    index: false,
+    follow: false
+  }
+};
 
 export default function MyPayLinkPage() {
   return (
