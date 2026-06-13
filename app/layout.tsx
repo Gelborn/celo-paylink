2d6fb14ed1b8f1ef4c6cad9c2ae91654c10da1c0
Gelborn <felipe-grasnievicz@hotmail.com>
2026-06-13 00:59:00 -0300

    Add MiniPay PayLink metadata keyword

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(publicEnv.appUrl),
   keywords: [
     "MiniPay",
+    "MiniPay PayLink",
     "MiniPay payments",
     "PayLink",
     "Celo",
